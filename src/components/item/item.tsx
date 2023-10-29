77f0ae7d849a0d29f6652f8be7a400305d76e2d6
Pavel <Ptashkaaaaaa@gmail.com>
2023-10-29 23:53:17 +0100

    fix: fix item to class component

--- src/components/item/item.tsx
+++ src/components/item/item.tsx
@@ -1,18 +1,23 @@
 import './item.scss';
-import React from 'react';
+import { Component } from 'react';
 import { CharacterInterface } from '../../services/rick-api/rick-api.models.ts';
 
 interface ItemListProps {
   item: CharacterInterface;
 }
-const Item: React.FC<ItemListProps> = ({ item }) => {
-  return (
-    <div className={'item'}>
-      <h4>{item.name}</h4>
-      <img className="item__img" src={item.image} alt={item.name} />
-      <p>Gender: {item.gender}</p>
-    </div>
-  );
-};
+
+class Item extends Component<ItemListProps> {
+  render() {
+    const { item } = this.props;
+
+    return (
+      <div className="item">
+        <h4>{item.name}</h4>
+        <img className="item__img" src={item.image} alt={item.name} />
+        <p>Gender: {item.gender}</p>
+      </div>
+    );
+  }
+}
 
 export default Item;
