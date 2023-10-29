771dce26b948a06a1e32cadb02f54b9306714d0d
Pavel <Ptashkaaaaaa@gmail.com>
2023-10-30 00:05:06 +0100

    fix: fix app to class component

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,45 @@
 import './App.scss';
 import Header from './components/header/header.tsx';
 import Footer from './components/footer/footer.tsx';
-import { useState } from 'react';
+import { Component } from 'react';
 import { CharacterResponseInterface } from './services/rick-api/rick-api.models.ts';
 import MainPage from './pages/main/main-page.tsx';
 
-function App() {
-  const [rickResponse, setRickResponse] = useState<CharacterResponseInterface | null>(null);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const setResponseCB = (res: CharacterResponseInterface | null) => {
-    setRickResponse(res);
+interface AppState {
+  rickResponse: CharacterResponseInterface | null;
+  isLoading: boolean;
+}
+
+class App extends Component<AppState, AppState> {
+  constructor(props: AppState) {
+    super(props);
+    this.state = {
+      rickResponse: null,
+      isLoading: false,
+    };
+  }
+
+  setResponseCB = (res: CharacterResponseInterface | null) => {
+    this.setState({ rickResponse: res });
   };
-  const setIsLoadingCB = (isLoading: boolean) => {
-    setIsLoading(isLoading);
+
+  setIsLoadingCB = (isLoading: boolean) => {
+    this.setState({ isLoading });
   };
-  return (
-    <>
-      <Header setResponseCB={setResponseCB} setIsLoadingCB={setIsLoadingCB} />
-      <main className={'main'}>
-        {isLoading ? 'loading' : <MainPage rickResponse={rickResponse} />}
-      </main>
-      <Footer />
-    </>
-  );
+
+  render() {
+    const { rickResponse, isLoading } = this.state;
+
+    return (
+      <div>
+        <Header setResponseCB={this.setResponseCB} setIsLoadingCB={this.setIsLoadingCB} />
+        <main className="main">
+          {isLoading ? 'loading' : <MainPage rickResponse={rickResponse} />}
+        </main>
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
