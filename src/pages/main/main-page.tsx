69eca68717c1cb9b50a7d9fa637c0f7914817da6
Pavel <Ptashkaaaaaa@gmail.com>
2023-10-30 00:00:39 +0100

    fix: fix main-page to class component

--- src/pages/main/main-page.tsx
+++ src/pages/main/main-page.tsx
@@ -1,16 +1,22 @@
 import './main-page.scss';
 import ItemList from '../../components/item-list/item-list.tsx';
-import React from 'react';
+import { Component } from 'react';
 import { CharacterResponseInterface } from '../../services/rick-api/rick-api.models.ts';
 
 interface MainPageProps {
   rickResponse: CharacterResponseInterface | null;
 }
 
-const MainPage: React.FC<MainPageProps> = ({ rickResponse }) => {
-  if (!rickResponse) return <h1>No results...</h1>;
+class MainPage extends Component<MainPageProps> {
+  render() {
+    const { rickResponse } = this.props;
 
-  return <ItemList rickResponse={rickResponse} />;
-};
+    if (!rickResponse) {
+      return <h1>No results...</h1>;
+    }
+
+    return <ItemList rickResponse={rickResponse} />;
+  }
+}
 
 export default MainPage;
