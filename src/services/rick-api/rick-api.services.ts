acd7d088f5f33fbc7016da39db87137744bc8415
Pavel <Ptashkaaaaaa@gmail.com>
2023-10-31 16:07:27 +0100

    fix: is husky fork?

--- src/services/rick-api/rick-api.services.ts
+++ src/services/rick-api/rick-api.services.ts
@@ -5,7 +5,9 @@ export class RickApiServices {
   public static async characterSearch(searchWord: string, page?: number) {
     try {
       const response = await axios.get<CharacterResponseInterface>(
-        `https://rickandmortyapi.com/api/character/?name=${searchWord}&page=${page || 1}`
+        `https://rickandmortyapi.com/api/character/?name=${searchWord}&page=${
+          page || 1
+        }`
       );
       return response.data;
     } catch (error) {
