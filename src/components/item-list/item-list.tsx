47c6209389a50d12e57d5b1edf787fbeaf1144f4
Pavel <Ptashkaaaaaa@gmail.com>
2023-11-05 13:30:19 +0100

    fix: add item-list to fc

--- src/components/item-list/item-list.tsx
+++ src/components/item-list/item-list.tsx
@@ -1,5 +1,5 @@
 import './item-list.scss';
-import { Component } from 'react';
+import React from 'react';
 import { CharacterResponseInterface } from '../../services/rick-api/rick-api.models.ts';
 import Item from '../item/item.tsx';
 
@@ -7,19 +7,16 @@ interface ItemListProps {
   rickResponse: CharacterResponseInterface;
 }
 
-class ItemList extends Component<ItemListProps> {
-  render() {
-    const { rickResponse } = this.props;
-    const itemsArray = rickResponse.results;
+const ItemList: React.FC<ItemListProps> = ({ rickResponse }) => {
+  const itemsArray = rickResponse.results;
 
-    return (
-      <div className="item-list">
-        {itemsArray.map((item) => (
-          <Item item={item} key={item.id} />
-        ))}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="item-list">
+      {itemsArray.map((item) => (
+        <Item item={item} key={item.id} />
+      ))}
+    </div>
+  );
+};
 
 export default ItemList;
