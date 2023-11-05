388341c4b1efa3d24468160c5f2a39c0c11c85eb
Pavel <Ptashkaaaaaa@gmail.com>
2023-11-05 13:31:36 +0100

    fix: add main-page to fc

--- src/pages/main/main-page.tsx
+++ src/pages/main/main-page.tsx
@@ -1,22 +1,16 @@
 import './main-page.scss';
 import ItemList from '../../components/item-list/item-list.tsx';
-import { Component } from 'react';
+import React from 'react';
 import { CharacterResponseInterface } from '../../services/rick-api/rick-api.models.ts';
 
 interface MainPageProps {
   rickResponse: CharacterResponseInterface | null;
 }
 
-class MainPage extends Component<MainPageProps> {
-  render() {
-    const { rickResponse } = this.props;
+const MainPage: React.FC<MainPageProps> = ({ rickResponse }) => {
+  if (!rickResponse) return <h1>No results...</h1>;
 
-    if (!rickResponse) {
-      return <h1>No results...</h1>;
-    }
-
-    return <ItemList rickResponse={rickResponse} />;
-  }
-}
+  return <ItemList rickResponse={rickResponse} />;
+};
 
 export default MainPage;
