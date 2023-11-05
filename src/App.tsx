0b7ef1144122a0af537c20aafce14f1cc423247d
Pavel <Ptashkaaaaaa@gmail.com>
2023-11-05 13:33:11 +0100

    fix: add app to fc

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,34 @@
 import './App.scss';
 import Header from './components/header/header.tsx';
 import Footer from './components/footer/footer.tsx';
-import { Component } from 'react';
+import React, { useState } from 'react';
 import { CharacterResponseInterface } from './services/rick-api/rick-api.models.ts';
 import MainPage from './pages/main/main-page.tsx';
 
-interface AppState {
-  rickResponse: CharacterResponseInterface | null;
-  isLoading: boolean;
-}
 interface AppProps {}
 
-class App extends Component<AppProps, AppState> {
-  constructor(props: AppProps) {
-    super(props);
-    this.state = {
-      rickResponse: null,
-      isLoading: false,
-    };
-  }
-  setResponseCB = (res: CharacterResponseInterface | null) => {
-    this.setState({ rickResponse: res });
+const App: React.FC<AppProps> = () => {
+  const [rickResponse, setRickResponse] =
+    useState<CharacterResponseInterface | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const setResponseCB = (res: CharacterResponseInterface | null) => {
+    setRickResponse(res);
   };
 
-  setIsLoadingCB = (isLoading: boolean) => {
-    this.setState({ isLoading });
+  const setIsLoadingCB = (isLoading: boolean) => {
+    setIsLoading(isLoading);
   };
 
-  render() {
-    const { rickResponse, isLoading } = this.state;
-
-    return (
-      <>
-        <Header
-          setResponseCB={this.setResponseCB}
-          setIsLoadingCB={this.setIsLoadingCB}
-        />
-        <main className="main">
-          {isLoading ? 'loading' : <MainPage rickResponse={rickResponse} />}
-        </main>
-        <Footer />
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <Header setResponseCB={setResponseCB} setIsLoadingCB={setIsLoadingCB} />
+      <main className="main">
+        {isLoading ? 'loading' : <MainPage rickResponse={rickResponse} />}
+      </main>
+      <Footer />
+    </>
+  );
+};
 
 export default App;
