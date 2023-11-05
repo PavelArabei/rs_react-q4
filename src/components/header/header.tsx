2cfd68a2b88bc3e622bb179af4e1601b923a9e96
Pavel <Ptashkaaaaaa@gmail.com>
2023-11-05 13:28:44 +0100

    fix: add item and header to fc

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,5 +1,5 @@
 import './header.scss';
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { RickApiServices } from '../../services/rick-api/rick-api.services.ts';
 import { CharacterResponseInterface } from '../../services/rick-api/rick-api.models.ts';
 
@@ -8,83 +8,67 @@ interface HeaderProps {
   setIsLoadingCB: (isLoading: boolean) => void;
 }
 
-interface HeaderState {
-  searchText: string;
-  error: boolean;
-}
+const Header: React.FC<HeaderProps> = ({ setResponseCB, setIsLoadingCB }) => {
+  const [searchText, setSearchText] = useState<string>('');
+  const [error, setError] = useState<boolean>(false);
 
-class Header extends Component<HeaderProps, HeaderState> {
-  constructor(props: HeaderProps) {
-    super(props);
-
-    this.state = {
-      searchText: '',
-      error: false,
-    };
-  }
-
-  componentDidMount() {
+  useEffect(() => {
     const savedSearchText = localStorage.getItem('searchTerm');
-    if (savedSearchText) this.setState({ searchText: savedSearchText });
-    this.handleSearch();
-  }
+    if (savedSearchText) setSearchText(savedSearchText);
+    handleSearch().then();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-  handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
-    this.setState({ searchText: e.target.value.trim() });
-  };
-
-  handleSearch = async () => {
-    const { searchText } = this.state;
-    const { setResponseCB, setIsLoadingCB } = this.props;
-
+  const handleSearch = async (): Promise<void> => {
     if (searchText) {
       localStorage.setItem('searchTerm', searchText);
     }
 
     setIsLoadingCB(true);
 
-    const response: CharacterResponseInterface | undefined =
-      await RickApiServices.characterSearch(searchText);
+    try {
+      const response: CharacterResponseInterface | undefined =
+        await RickApiServices.characterSearch(searchText);
 
-    if (response) {
-      setResponseCB(response);
-    } else {
-      setResponseCB(null);
+      if (response) setResponseCB(response);
+      else setResponseCB(null);
+    } catch {
+      handleError();
     }
 
     setIsLoadingCB(false);
   };
 
-  setError = () => {
-    this.setState({ error: true });
-    console.log(this.state);
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchText(e.target.value.trim());
   };
 
-  render() {
-    const { searchText } = this.state;
-    if (this.state.error) {
-      throw new Error('Something go wrong');
-    }
-    return (
-      <>
-        <button className={'throw-error-button'} onClick={this.setError}>
-          BIG BAD BUTTON TO THROW ERROR
-        </button>
-        <h1>RICK AND MORTY</h1>
-        <header className={'header'}>
-          <input
-            value={searchText}
-            type="text"
-            onChange={this.handleSearchChange}
-            className={'header__input'}
-          />
-          <button onClick={this.handleSearch} className={'header__button'}>
-            click me!
-          </button>
-        </header>
-      </>
-    );
+  const handleError = () => {
+    setError(true);
+  };
+
+  if (error) {
+    throw new Error('Something go wrong');
   }
-}
+  return (
+    <>
+      <button className="throw-error-button" onClick={handleError}>
+        BIG BAD BUTTON TO THROW ERROR
+      </button>
+      <h1>RICK AND MORTY</h1>
+      <header className="header">
+        <input
+          value={searchText}
+          type="text"
+          onChange={handleSearchChange}
+          className="header__input"
+        />
+        <button onClick={handleSearch} className="header__button">
+          click me!
+        </button>
+      </header>
+    </>
+  );
+};
 
 export default Header;
